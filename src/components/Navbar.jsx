5bfb6b47b8c91db351a39ecefc8e9137662c3830
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-01-08 19:52:32 +0100

    add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import { navLinks } from "#constants/index.js";
 import React from "react";
+import dayjs from "dayjs";
+
+import { navIcons, navLinks } from "#constants";
 
 const Navbar = () => {
     return (
@@ -15,6 +17,21 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img
+                                src={img}
+                                className="icon-hover"
+                                alt={`icon-${id}`}
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
             </div>
         </nav>
     );
