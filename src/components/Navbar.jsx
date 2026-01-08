13926ca16a10b69d7683d06d3a8da72909f11e63
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-01-08 19:41:11 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { navLinks } from "#constants/index.js";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">Gegë's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
