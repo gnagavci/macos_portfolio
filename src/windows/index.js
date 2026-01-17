841b3658c7de9e2a7cb6a6e9896854a6f4222a63
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-01-17 16:59:40 +0100

    add Text component for text window rendering

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,4 +2,5 @@ import Terminal from "./Terminal.jsx";
 import Safari from "./Safari.jsx";
 import Resume from "./Resume.jsx";
 import Finder from "./Finder.jsx";
-export { Terminal, Safari, Resume, Finder };
+import Text from "./Text.jsx";
+export { Terminal, Safari, Resume, Finder, Text };
