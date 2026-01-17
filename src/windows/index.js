b84189e24ad9d7ccf60edce17001ee38e1f35188
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-01-17 19:04:18 +0100

    implement Photos component

--- src/windows/index.js
+++ src/windows/index.js
@@ -5,4 +5,5 @@ import Finder from "./Finder.jsx";
 import Text from "./Text.jsx";
 import Image from "./Image.jsx";
 import Contact from "./Contact.jsx";
-export { Terminal, Safari, Resume, Finder, Text, Image, Contact };
+import Photos from "./Photos.jsx";
+export { Terminal, Safari, Resume, Finder, Text, Image, Contact, Photos };
