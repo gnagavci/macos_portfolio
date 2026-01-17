929228dc208255f355ccdd6babfbac0b72825392
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-01-17 19:26:27 +0100

    add deployment changes

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,26 @@ import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/macos_portfolio/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             "#components": resolve(
                 dirname(fileURLToPath(import.meta.url)),
-                "src/components"
+                "src/components",
             ),
             "#constants": resolve(
                 dirname(fileURLToPath(import.meta.url)),
-                "src/constants"
+                "src/constants",
             ),
             "#store": resolve(
                 dirname(fileURLToPath(import.meta.url)),
-                "src/store"
+                "src/store",
             ),
             "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "src/hoc"),
             "#windows": resolve(
                 dirname(fileURLToPath(import.meta.url)),
-                "src/windows"
+                "src/windows",
             ),
         },
     },
